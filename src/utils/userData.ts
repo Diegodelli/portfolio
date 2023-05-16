73b318d03b84164317a7923a9d54c11190bd7200
Diego Delli Colli <dellicolli@gmail.com>
2023-05-16 14:15:23 -0300

    first commit

---
+++ src/utils/userData.ts
@@ -0,0 +1,9 @@
+export const userData = {
+  nameUser: "Diego Delli Colli Ramos",
+  githubUser: "Diegodelli",
+  linkedinUser: "diegodcramos",
+  whatsappNumber: "+5511982446057",
+  emailUser: "dellicolli89@gmail.com",
+  instagramUser: "yellowcury89",
+  facebookUser: "profile.php?id=100075586237990&mibextid=ZbWKwL",
+};
